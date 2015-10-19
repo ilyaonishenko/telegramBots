aa1cc3295aa8a92ca319e764c6c1fd0df5ac337a
wopqw <wopqw@yandex.ru>
2015-10-20 00:02:56 +0300

    working glassfish server

--- vk-message-bot/glassfish-server/src/main/java/me/vkServMessBot/controllers/RedirectOne.java
+++ vk-message-bot/glassfish-server/src/main/java/me/vkServMessBot/controllers/RedirectOne.java
@@ -1,7 +1,33 @@
 package me.vkServMessBot.controllers;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by woqpw on 19.10.15.
  */
+@Path("/redir")
 public class RedirectOne {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello World RESTful Jersey!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello World RESTful Jersey"
+                + "</hello>";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello World RESTful Jersey"
+                + "</title>" + "<body><h1>" + "Hello World RESTful Jersey"
+                + "</body></h1>" + "</html> ";
+    }
 }

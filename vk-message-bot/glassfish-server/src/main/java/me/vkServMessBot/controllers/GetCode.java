5e9becee485672d62d52e9c37091df20a0efa2b6
wopqw <wopqw@yandex.ru>
2015-10-20 02:02:29 +0300

    vk pidory

---
+++ vk-message-bot/glassfish-server/src/main/java/me/vkServMessBot/controllers/GetCode.java
@@ -0,0 +1,11 @@
+package me.vkServMessBot.controllers;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by woqpw on 20.10.15.
+ */
+@Path("/getcode")
+public class GetCode {
+
+}
--- vk-message-bot/glassfish-server/src/main/java/me/vkServMessBot/controllers/RedirectOne.java
+++ vk-message-bot/glassfish-server/src/main/java/me/vkServMessBot/controllers/RedirectOne.java
@@ -3,6 +3,7 @@ package me.vkServMessBot.controllers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -16,18 +17,28 @@ public class RedirectOne {
         return "Hello World RESTful Jersey!";
     }
 
-    @GET
+    /*@GET
     @Produces(MediaType.TEXT_XML)
     public String sayXMLHello() {
         return "<?xml version=\"1.0\"?>" + "<hello> Hello World RESTful Jersey"
                 + "</hello>";
-    }
+    }*/
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String sayHtmlHello() {
+        System.out.println("here");
+        String redirLink = "https://oauth.vk.com/authorize?display=page&client_id=5102517&scope=friends,photos,audio,video,messages&redirect_uri=http://localhost:8080/vkmesserv/redirect/redir/getcode&response_type=code&v=5.37";
+//        String redirLink = "https://oauth.vk.com/blank.html";
         return "<html> " + "<title>" + "Hello World RESTful Jersey"
-                + "</title>" + "<body><h1>" + "Hello World RESTful Jersey"
-                + "</body></h1>" + "</html> ";
+                + "</title>" + "<body><a link href="+redirLink+">" + "go to vk auth"
+                + "</a></body>" + "</html> ";
+    }
+    @Path("/getcode")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getCode(@QueryParam("code") String code){
+        System.out.println(code);
+        return "<html><body><h1>"+code+"</h1></body></html>";
     }
 }

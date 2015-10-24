414de4bc101df6811038d134d121810b088cde46
wopqw <wopqw@yandex.ru>
2015-10-24 17:38:43 +0300

    adding logging and all in classes

--- insta-bot/glassfish-server/src/main/java/me/glassfish/controllers/Application.java
+++ insta-bot/glassfish-server/src/main/java/me/glassfish/controllers/Application.java
@@ -3,6 +3,10 @@ package me.glassfish.controllers;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import me.glassfish.models.AppProperties;
+import me.glassfish.models.User;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,13 +16,14 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/redirect")
 public class Application {
+    private Logger logger = LoggerFactory.getLogger(Application.class);
     String code;
     com.sun.jersey.api.client.Client client = com.sun.jersey.api.client.Client.create();
     WebResource webResource;
-    //TODO убрать необходимость кликать по ссылочке вручную. Сделать как в setcode.
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String redirectLink(){
+        logger.info("glassfishes","in meth redirectLink");
         final String ending = "https://api.instagram.com/oauth/authorize/?";
         String redirectLink = ending+ AppProperties.getClient_id()+"&"+AppProperties.getRedirect_uri()+"&"+AppProperties.getResponse_type();
         return "<html><body>"+"<a link href="+redirectLink+">go to inst auth2</a>"+"</body></html>";
@@ -27,13 +32,19 @@ public class Application {
     @Path("/code")
     @Produces(MediaType.TEXT_PLAIN)
     public String setCode(@QueryParam("code")String code){
+        logger.info("glassfishes");
         final String ending = "https://api.instagram.com/oauth/access_token";
         this.code = code;
+        logger.info("code {}",code);
         String object = AppProperties.getClient_id()+"&"+AppProperties.getClient_secret()+"&"+AppProperties.getGrant_type()+"&"+AppProperties.getRedirect_uri()+"&code="+code;
         webResource = client.resource(ending);
         ClientResponse response = webResource.type("application/x-www-form-urlencoded").post(ClientResponse.class,object);
         String newObject = response.getEntity(String.class);
-        System.out.println(newObject);
+        JSONObject jsonObject = new JSONObject(newObject);
+        JSONObject userJson = jsonObject.getJSONObject("user");
+        User user = new User(userJson.get("username").toString(),userJson.get("bio").toString(),userJson.get("website").toString(),
+                userJson.get("profile_picture").toString(),userJson.get("full_name").toString(),Integer.parseInt(userJson.getString("id")));
+        logger.info("userClass {}",user);
         return newObject;
     }
 }
--- insta-bot/glassfish-server/src/main/java/me/glassfish/models/User.java
+++ insta-bot/glassfish-server/src/main/java/me/glassfish/models/User.java
@@ -1,7 +1,89 @@
 package me.glassfish.models;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by woqpw on 24.10.15.
  */
 public class User {
+
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private int id;
+    private String username;
+    private String bio;
+    private String website;
+    private String profile_picture;
+    private String full_name;
+    private int instId;
+
+    public User (String username,String bio,String website,String profile_picture,
+                 String full_name,int inst_id){
+        this.username = username;
+        this.bio = bio;
+        this.website = website;
+        this.profile_picture = profile_picture;
+        this.full_name = full_name;
+        this.instId = inst_id;
+        this.id = count.incrementAndGet();
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProfile_picture() {
+        return profile_picture;
+    }
+
+    public void setProfile_picture(String profile_picture) {
+        this.profile_picture = profile_picture;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public int getInstId() {
+        return instId;
+    }
+
+    public void setInstId(int instId) {
+        this.instId = instId;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String toString(){
+        return new StringBuffer("username: ").append(this.username)
+                .append(" bio: ").append(this.bio)
+                .append(" website: ").append(this.website)
+                .append(" profile_picture: ").append(this.profile_picture)
+                .append(" full_name: ").append(this.full_name)
+                .append(" instId: ").append(this.instId).toString();
+    }
 }
